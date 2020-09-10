e0300c871870ccfcf98ad80e35f06f7a6950c3ac
Chyngyz1009 <chika_0410@inbox.ru>
2020-09-10 19:31:56 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,87 @@
+package com.company;
+
+public class Main {
+    String name = "Chyngyz";
+
+
+    public static void main(String[] args) {
+
+        int temperature = -25;
+        int age = 44;
+
+        if (temperature < -20 & temperature < 30 & (age > 20 & age < 45)) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age < 20 & temperature > 0 & temperature < 28) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age > 45 & temperature > -10 & temperature < 25
+        ) {
+            System.out.println("Можно идти гулять! ");
+        } else {
+            System.out.println("Оставайтесь дома!");
+        }
+
+        int temperature1 = 15;
+        int age1 = 21;
+
+        if (temperature1 < -20 & temperature1 < 30 & (age1 > 20 & age1 < 45)) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age1 < 20 & temperature1 > 0 & temperature1 < 28) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age1 > 45 & temperature1 > -10 & temperature1 < 25
+        ) {
+            System.out.println("Можно идти гулять! ");
+        } else {
+            System.out.println("Оставайтесь дома!");
+        }
+
+        int temperature2 = 27;
+        int age2 = 78;
+
+        if (temperature2 < -20 & temperature2 < 30 & (age2 > 20 & age2 < 45)) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age2 < 20 & temperature2 > 0 & temperature2 < 28) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age2 > 45 & temperature2 > -10 & temperature2 < 25
+        ) {
+            System.out.println("Можно идти гулять! ");
+        } else {
+            System.out.println("Оставайтесь дома!");
+        }
+        int temperature3 = 37;
+        int age3 = 56;
+
+        if (temperature3 < -20 & temperature3 < 30 & (age3 > 20 & age3 < 45)) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age3 < 20 & temperature3 > 0 & temperature3 < 28) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age3 > 45 & temperature3 > -10 & temperature3 < 25
+        ) {
+            System.out.println("Можно идти гулять! ");
+        } else {
+            System.out.println("Оставайтесь дома!");
+        }
+        int temperature4 = 22;
+        int age4 = 18;
+
+        if (temperature4 < -20 & temperature4 < 30 & (age4 > 20 & age4 < 45)) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age4 < 20 & temperature4 > 0 & temperature4 < 28) {
+            System.out.println("Можно идти гулять!");
+        }
+        if (age4 > 45 & temperature4 > -10 & temperature4 < 25
+        ) {
+            System.out.println("Можно идти гулять! ");
+        } else {
+            System.out.println("Оставайтесь дома!");
+        }
+    }
+}
